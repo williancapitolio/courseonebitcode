865ea053ed54971579c23350f26269706071a83d
Willian <williansilvap25@gmail.com>
2023-02-28 15:54:13 -0300

    task 3 ok

---
+++ js/logic_programming/task_3/scripts/script.js
@@ -0,0 +1,73 @@
+/*
+
+## Conversor de Medidas
+
+Escreva um programa em javascript que funcione como um conversos de medidas. O programa deverá pedir por um valor em metros e então dar a opção de escolher para qual unidade de medida esse valor deve ser convertido. As opções são:
+
+- milímetro (mm)
+- centímetro (cm)
+- decímetro (dm)
+- decâmetro (dam)
+- hectômetro (hm)
+- quilômetro (km)
+
+O programa deve então converter a medida de acordo com a opção escolhida e exibir o resultado.
+
+O programa também deve exibir uma mensagem de “Opção inválida” caso o usuário insira uma opção diferente das disponíveis (use o break e o default para isso)
+
+*/
+
+"use strict";
+
+const value = prompt(
+    "Exercício 3" +
+    "\n\nDigite um valor um metros:"
+);
+
+let valueNumeric = parseFloat(value);
+
+const choice = prompt(
+    "Deseja converter " + valueNumeric + " metros (m) para qual medida?\n" +
+    "\nDigite 1 - para milímetro (mm)" +
+    "\nDigite 2 - para centímetro (cm)" +
+    "\nDigite 3 - para decímetro (dm)" +
+    "\nDigite 4 - para decâmetro (dam)" +
+    "\nDigite 5 - para hectômetro (hm)" +
+    "\nDigite 6 - para quilômetro (km)" +
+    "\nDigite 7 - para finalizar"
+);
+
+const choiceNumeric = parseFloat(choice);
+
+switch (choiceNumeric) {
+    case 1:
+        valueNumeric /= 1000;
+        alert("A conversão resultou em " + valueNumeric + " milímetros (mm).");
+        break;
+    case 2:
+        valueNumeric /= 100;
+        alert("A conversão resultou em " + valueNumeric + " centímetros (cm).");
+        break;
+    case 3:
+        valueNumeric /= 10;
+        alert("A conversão resultou em " + valueNumeric + " decímetros (dm).");
+        break;
+    case 4:
+        valueNumeric *= 10;
+        alert("A conversão resultou em " + valueNumeric + " decâmetros (dam).");
+        break;
+    case 5:
+        valueNumeric *= 100;
+        alert("A conversão resultou em " + valueNumeric + " hectômetros (hm).");
+        break;
+    case 6:
+        valueNumeric *= 1000;
+        alert("A conversão resultou em " + valueNumeric + " quilômetros (km).");
+        break;
+    case 7:
+        alert("Finalizando...");
+        break;
+    default:
+        alert("Opção inválida!");
+        location.reload();
+};
