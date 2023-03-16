acff9b4908e4a67febf18b46aded79f93a47b606
Willian <williansilvap25@gmail.com>
2023-03-16 00:14:31 -0300

    add event listener for submit add technology

--- js/2_dom/task_12/scripts/script.js
+++ js/2_dom/task_12/scripts/script.js
@@ -26,7 +26,79 @@ const devForm = document.getElementById("dev-form");
 
 devForm.addEventListener("submit", function (event) {
     event.preventDefault();
+
+    const devName = event.target.devname.value;
+
+    addTechnology();
 });
+
+function addTechnology() {
+    const labelTechnologyName = document.createElement("label");
+    labelTechnologyName.setAttribute("for", "technology-name");
+    labelTechnologyName.innerText = "Tecnologia: ";
+
+    const inputTechnologyName = document.createElement("input");
+    inputTechnologyName.type = "text";
+    inputTechnologyName.name = "technology-name";
+    inputTechnologyName.id = "technology-name";
+    inputTechnologyName.placeholder = "Digite o nome da tecnologia";
+
+    const labelExperience = document.createElement("label");
+    labelExperience.setAttribute("for", "technology-experience");
+    labelExperience.innerText = "Tempo de experiência";
+
+    const inputRadioFirst = document.createElement("input");
+    inputRadioFirst.type = "radio";
+    inputRadioFirst.name = "technology-experience";
+    inputRadioFirst.id = "first";
+    inputRadioFirst.value = "0-2 anos";
+    const labelRadioFirst = document.createElement("label");
+    labelRadioFirst.setAttribute("for", "first");
+    labelRadioFirst.innerText = "0-2 anos";
+
+    const inputRadioSecond = document.createElement("input");
+    inputRadioSecond.type = "radio";
+    inputRadioSecond.name = "technology-experience";
+    inputRadioSecond.id = "second";
+    inputRadioSecond.value = "3-4 anos";
+    const labelRadioSecond = document.createElement("label");
+    labelRadioSecond.setAttribute("for", "second");
+    labelRadioSecond.innerText = "3-4 anos";
+
+    const inputRadioThird = document.createElement("input");
+    inputRadioThird.type = "radio";
+    inputRadioThird.name = "technology-experience";
+    inputRadioThird.id = "third";
+    inputRadioThird.value = "5+ anos";
+    const labelRadioThird = document.createElement("label");
+    labelRadioThird.setAttribute("for", "third");
+    labelRadioThird.innerText = "5+ anos";
+
+    const devTechnologies = document.getElementById("dev-technologies");
+
+    devTechnologies.appendChild(labelTechnologyName);
+    devTechnologies.appendChild(inputTechnologyName);
+    devTechnologies.appendChild(document.createElement("br"));
+    devTechnologies.appendChild(document.createElement("br"));
+
+    devTechnologies.appendChild(labelExperience);
+    devTechnologies.appendChild(document.createElement("br"));
+
+    devTechnologies.appendChild(inputRadioFirst);
+    devTechnologies.appendChild(labelRadioFirst);
+    devTechnologies.appendChild(document.createElement("br"));
+
+    devTechnologies.appendChild(inputRadioSecond);
+    devTechnologies.appendChild(labelRadioSecond);
+    devTechnologies.appendChild(document.createElement("br"));
+
+    devTechnologies.appendChild(inputRadioThird);
+    devTechnologies.appendChild(labelRadioThird);
+    devTechnologies.appendChild(document.createElement("br"));
+
+    devTechnologies.appendChild(document.createElement("br"));
+    devTechnologies.appendChild(document.createElement("br"));
+};
 
 const devInfo = document.getElementById("dev-info");
 
