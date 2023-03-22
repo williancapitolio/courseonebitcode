464e49adf8c33e2ece059cafc60b12212078e036
Willian <williansilvap25@gmail.com>
2023-03-22 00:05:17 -0300

    add task 13

---
+++ js/2_dom/task_13/scripts/script.js
@@ -0,0 +1,26 @@
+/*
+
+## Tic-Tac-Toe / Jogo da Velha
+
+Construa um jogo tic-tac-toe, conhecido em português como jogo da velha, utilizando HTML, CSS e Javascript.
+Você pode desenvolver com liberdade, da forma que preferir, mas o jogo precisa atender aos seguintes requisitos:
+
+- Deve ser possível incluir os nomes dos dois jogadores;
+
+- O nome do jogador da vez deve ser mostrado na tela e alterado a medida que os turnos vão se alternando;
+
+- Um tabuleiro deve ser mostrado na tela e ser atualizado quando o jogador clicar na região que ele quer marcar;
+
+- Quando um jogador clicar no tabuleiro deve ser marcado um “X” ou “O” de acordo com o jogador da vez e não
+deve ser possível clicar naquela região novamente;
+
+- Quando um jogador ganhar seu nome deve ser mostrado na tela e as regiões da vitória devem ser destacadas de
+alguma forma;
+
+- Em caso de empate, uma mensagem de empate deve ser mostrada na tela;
+
+- Deve ser possível reiniciar o jogo para jogar novamente.
+
+*/
+
+"use strict";
