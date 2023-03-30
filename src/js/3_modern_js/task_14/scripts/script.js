beccdc2912e14b8d7ae34f37e00d1a38e3cc4781
Willian <williansilvap25@gmail.com>
2023-03-29 22:19:40 -0300

    weighted arithmetic mean ok

--- src/js/3_modern_js/task_14/scripts/script.js
+++ src/js/3_modern_js/task_14/scripts/script.js
@@ -2,3 +2,13 @@ const getSimpleArithmeticMean = (...values) => {
     let sumValues = values.reduce((accumulator, value) => (accumulator + value), 0);
     return sumValues /= values.length;
 };
+
+const getWeightedArithmeticMean = (...values) => {
+    const sumNumberPlusWeigh = values.reduce((accumulator, value) => (value.n * value.p) + accumulator, 0);
+    const sumWeights = values.reduce((accumulator, value) => (accumulator + value.p), 0);
+    return sumNumberPlusWeigh / sumWeights
+};
+
+console.log(getSimpleArithmeticMean(2, 6, 3, 7, 4));
+
+console.log(getWeightedArithmeticMean({ n: 7, p: 2 }, { n: 9, p: 5 }, { n: 3, p: 1 }));
