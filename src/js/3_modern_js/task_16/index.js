a6392802f588b3d418a0fad7a5e4a840ed537326
Willian <williansilvap25@gmail.com>
2023-04-04 16:58:10 -0300

    task 16 ok

--- src/js/3_modern_js/task_16/index.js
+++ src/js/3_modern_js/task_16/index.js
@@ -1,18 +1,42 @@
 "use strict";
 
-const datesWithDaysjs = birthday => {
-    const validBirthday = /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/;
+const dayjs = require("dayjs");
 
-    if (!validBirthday.test(birthday)) {
-        console.log(`${birthday} not a valid birthday. Try the format: dd/mm/yyyy`);
-        return;
+const datesWithDayjs = (dateOfBirth) => {
+    const validDateOfBirth = /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/;
+
+    if (!validDateOfBirth.test(dateOfBirth)) {
+        return console.log(`
+        - ${dateOfBirth} not a valid date of birth. Try the format: DD/MM/YYYY
+        `);
     };
 
-    console.log(birthday);
-    
-    /* const currentAge = "";
-    const nextBirthday = "";
-    const daysToNextBirthday = ""; */
+    const dateOfBirthArray = dateOfBirth.split("/");
+
+    const dateOfBirthResult = `${dateOfBirthArray[2]}-${dateOfBirthArray[1]}-${dateOfBirthArray[0]}`;
+
+    const birthdayDayjs = dayjs(dateOfBirthResult).format("YYYY-MM-DD");
+
+    const today = dayjs().format("YYYY-MM-DD");
+
+    const currentAge = dayjs(today).diff(dateOfBirthResult, "year");
+
+    const nextBirthday = dayjs(birthdayDayjs).add((currentAge + 1), "year").format("YYYY-MM-DD");
+
+    const daysToNextBirthday = dayjs(nextBirthday).diff(today, "day");
+
+    const todayFormat = dayjs(today).format("DD/MM/YYYY");
+
+    const nextBirthdayFormat = dayjs(nextBirthday).format("DD/MM/YYYY");
+
+    return console.log(`
+        - Today is ${todayFormat}
+        - Your birthday is ${dateOfBirth}
+        - Your age is ${currentAge}
+        - Your next birthday is ${nextBirthdayFormat}
+        - ${daysToNextBirthday} days until your next birthday
+    `);
 };
 
-datesWithDaysjs("25/10/1998");
+datesWithDayjs("25-10-1998");
+datesWithDayjs("25/10/1998");
