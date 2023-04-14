612b4247919bda10f24e9ebff89eec771071cb51
Willian <williansilvap25@gmail.com>
2023-04-13 23:36:28 -0300

    add babel loader

--- src/js/3_modern_js/task_17/webpack.config.js
+++ src/js/3_modern_js/task_17/webpack.config.js
@@ -1,14 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 module.exports = {
-    entry: {
-        index: "./src/index.js"
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "dist"),
     },
     mode: "production",
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", { targets: "defaults" }]
+                        ]
+                    }
+                }
             },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
         ],
     },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.min.css"
+        })
+    ]
 };
