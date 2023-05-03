8d832accf03e2c030e0ae299fa41d54f1049501a
Willian <williansilvap25@gmail.com>
2023-05-02 23:22:25 -0300

    test and ok

--- src/js/4_poo/task_20/src/index.js
+++ src/js/4_poo/task_20/src/index.js
@@ -3,3 +3,30 @@ import Input from "./classes/Input.js";
 import Label from "./classes/Label.js";
 import Form from "./classes/Form.js";
 
+const br = new Component("br");
+const hr = new Component("hr");
+
+const title = new Component("h2", "body", { textContent: "Olá mundo!" });
+console.log(title);
+title.render();
+title.tag = "h3";
+title.build().render();
+
+const form = new Form("body");
+
+const label = new Label("Nome: ", form, { htmlFor: "nameInput" });
+const input = new Input(form, { id: "nameInput", name: "name" });
+const button = new Input(form, { type: "button", value: "Enviar" });
+
+form.render();
+label.render();
+input.render();
+//button.render();
+
+form.addChildren(
+    br,
+    new Label("Data de Nascimento: ", form, { htmlFor: "birthdayInput" }),
+    new Input(form, { id: "birthdayInput", name: "birthday", type: "date" }),
+    hr,
+    button
+);
