4d2af9c52ad9a26fcfb14fca8b4b378f4a746bad
Willian <williansilvap25@gmail.com>
2023-05-02 22:54:56 -0300

    class Input revised

--- src/js/4_poo/task_20/src/classes/Input.js
+++ src/js/4_poo/task_20/src/classes/Input.js
@@ -1,16 +1,7 @@
 import Component from "./Component.js";
 
 export default class Input extends Component {
-    constructor(type, domElement) {
-        super(domElement = "input");
-
-        this.type = type;
-
-        this.type();
-    };
-
-    type() {
-        //return this.buildElement().type = type;
-        return console.log(`Teste teste: ${this.type}`);
+    constructor(parent, options) {
+        super("input", parent, options);
     };
 };
