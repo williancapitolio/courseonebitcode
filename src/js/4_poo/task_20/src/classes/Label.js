735569ebb1689a4c7df70523e602ee88f8393b5b
Willian <williansilvap25@gmail.com>
2023-05-02 22:58:54 -0300

    class Label revised

--- src/js/4_poo/task_20/src/classes/Label.js
+++ src/js/4_poo/task_20/src/classes/Label.js
@@ -1,14 +1,7 @@
 import Component from "./Component.js";
 
 export default class Label extends Component {
-    constructor(text, domElement) {
-        super(domElement = "label");
-
-        this.text = text;
-        this.setText = function () {
-            const element = this.buildElement();
-            element.innerText = this.text;
-            return document.getElementById("body").append(element);
-        };
+    constructor(text, parent, options) {
+        super("label", parent, Object.assign({}, options, { textContent: text }));
     };
 };
