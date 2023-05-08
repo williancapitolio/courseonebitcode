1339114b1577045ccee0f5be4d3f2bb881fa3cb8
Willian <williansilvap25@gmail.com>
2023-05-07 22:22:47 -0300

    add structure of transfer

---
+++ src/js/4_poo/task_22/entities/Transfer.js
@@ -0,0 +1,8 @@
+module.exports = class Transfer {
+    constructor(userSender, userRecipient, value) {
+        this.userSender = userSender;
+        this.userRecipient = userRecipient;
+        this.value = value;
+        this.createdAt = new Date();
+    }
+};
