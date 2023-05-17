c0a802a5dc9eb75fcc14f46f65f5da32ad83c0c9
Willian <williansilvap25@gmail.com>
2023-05-16 22:48:33 -0300

    update class User

--- src/js/4_poo/task_22/classes/entities/User.js
+++ src/js/4_poo/task_22/classes/entities/User.js
@@ -1,7 +1,9 @@
+const Account = require("./Account");
+
 module.exports = class User {
-    constructor(fullname, email, account) {
+    constructor(fullname, email) {
         this.fullname = fullname;
         this.email = email;
-        this.account = account;
+        this.account = new Account(fullname);
     };
 };
