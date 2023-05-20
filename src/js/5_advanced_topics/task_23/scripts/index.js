6dc9be9cab1d6ea633831585aad24999497a2343
Willian <williansilvap25@gmail.com>
2023-05-19 23:48:53 -0300

    validade email and password with regex ok

--- src/js/5_advanced_topics/task_23/scripts/index.js
+++ src/js/5_advanced_topics/task_23/scripts/index.js
@@ -21,4 +21,11 @@ const validations = (email, password) => {
     if (!validEmail.test(email)) {
         return "Invalid email! Try: xx@xx.xx";
     };
+
+    const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_!@#$%^&*.,?]).+$/;
+    if (!validPassword.test(password)) {
+        return "Invalid password! You need at leat 8 characters containing: 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character";
+    };
+
+    return true;
 };
