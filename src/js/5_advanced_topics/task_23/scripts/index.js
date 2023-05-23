e48b893b2b14a89666da7f4ee58b1c5dc1784106
Willian <williansilvap25@gmail.com>
2023-05-23 00:04:28 -0300

    task 23 done

--- src/js/5_advanced_topics/task_23/scripts/index.js
+++ src/js/5_advanced_topics/task_23/scripts/index.js
@@ -1,36 +1,59 @@
 const formRegister = document.getElementById("form-register");
-const btnSubmit = document.getElementById("btn-submit");
-const fullname = document.getElementById("fullname")
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+
 let statusResponse = "";
 
 formRegister.addEventListener("submit", e => {
-    e.preventDefault();
+    try {
+        e.preventDefault();
 
-    console.log(fullname.value);
-    console.log(email.value);
-    console.log(password.value);
+        const inputStatus = e.target.children["status-response"];
 
-    fullname.value = "";
-    email.value = "";
-    password.value = "";
+        const fullname = e.target.children["fullname"];
+        const email = e.target.children["email"];
+        const password = e.target.children["password"];
+
+        if (!fullname.value || !email.value || !password.value) {
+            statusResponse = "Error: Fill all fields!";
+            return inputStatus.value = statusResponse;
+        };
+
+        if (!validateEmail(email.value)) {
+            statusResponse = "Error: Invalid email! Try: xx@xx.xx";
+            return inputStatus.value = statusResponse;
+        };
+
+        if (!validatePassword(password.value)) {
+            statusResponse = "Error: Invalid password!";
+            return inputStatus.value = statusResponse;
+        };
+
+        fullname.value = "";
+        email.value = "";
+        password.value = "";
+
+        statusResponse = "Sucess: Registration done!";
+        inputStatus.value = statusResponse;
+    } catch (e) {
+        console.log(e.message);
+        statusResponse = "Error: A system error occurred, please try again later.";
+        document.getElementById("status-response").value = statusResponse;
+    };
 });
 
 const validateEmail = (email) => {
     const validEmail = /^([a-z0-9?_.]{2,})+@([a-z]{2,})+.([a-z]{2,})$/i;
     if (!validEmail.test(email)) {
-        return statusResponse = "Error: Invalid email! Try: xx@xx.xx";
+        return false;
     };
 
-    return statusResponse = "Success: The email was accepted";
+    return true;
 };
 
 const validatePassword = (password) => {
     const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_!@#$%^&*.,?]).+$/;
     if (!validPassword.test(password)) {
-        return statusResponse = "Invalid password! You need at leat 8 characters containing: 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character";
+        return false;
     };
 
-    return statusResponse = "Success: The password was accepted";
+    return true;
 };
