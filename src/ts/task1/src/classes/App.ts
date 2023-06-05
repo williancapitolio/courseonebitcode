adcefd8a150bae0f778bb4e512dc01d2a2c3eb4b
Willian <williansilvap25@gmail.com>
2023-06-05 02:16:15 -0300

    add function to get spaceship by name

--- src/ts/task1/src/classes/App.ts
+++ src/ts/task1/src/classes/App.ts
@@ -1,14 +1,18 @@
 import Spaceship from "./entities/Spaceship";
 
 export default class App {
-  private static spaceships: Object[];
+  private static spaceships: [String, String, Number, String[], Boolean];
 
-  static get listSpaceships(): Object[] {
+  static get listSpaceships(): [String, String, Number, String[], Boolean] {
     return App.spaceships;
   }
 
   static createSpaceship(name: String, pilot: String, crewLimit: Number) {
     const newSpaceship = new Spaceship(name, pilot, crewLimit);
-    App.spaceships.push(newSpaceship);
+    App.spaceships.push(newSpaceship.name, newSpaceship.pilot, newSpaceship.crewLimit, newSpaceship.crew, newSpaceship.inMission);
   };
+
+  static findSpaceshipByName(spaceshipName: String) {
+    return App.spaceships.find(n => n === spaceshipName)
+  }
 };
