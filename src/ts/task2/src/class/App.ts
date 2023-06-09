45a1377003c3d58b4f64e6856ab1d3471fcab2a4
Willian <williansilvap25@gmail.com>
2023-06-09 15:13:37 -0300

    indented code

--- src/ts/task2/src/class/App.ts
+++ src/ts/task2/src/class/App.ts
@@ -32,17 +32,25 @@ export default class App {
   static listPlanets() {
     let list = `Planetas Cadastrados:\n\n`;
 
-    App.planets.length > 0 ? App.planets.forEach((planet, i
-      ) => {
-        list += `Planeta: ${i + 1
-        }\nNome: ${planet.name
-        }\nCoordenadas:\n${planet.coordinates.reduce(
-          (acc, cord, i) => acc + ("- Coordenada " + (i + 1) + ". " + cord + "\n"), "")
-        }Situação: ${planet.situation
-        }\nSatélites:\n${planet.satellites.length > 0 ? planet.satellites.reduce(
-          (acc, sat, i) => acc + ("- Satélite " + (i + 1 )+ ". " + sat + "\n"),"") : "Nenhum Satélite Cadastrado\n"
-        }\n`;
-    }) : list += "Nenhum Planeta Cadastrado!";
+    App.planets.length > 0
+      ? App.planets.forEach((planet, i) => {
+          list += `Planeta: ${i + 1}\nNome: ${
+            planet.name
+          }\nCoordenadas:\n${planet.coordinates.reduce(
+            (acc, cord, i) =>
+              acc + ("- Coordenada " + (i + 1) + ". " + cord + "\n"),
+            ""
+          )}Situação: ${planet.situation}\nSatélites:\n${
+            planet.satellites.length > 0
+              ? planet.satellites.reduce(
+                  (acc, sat, i) =>
+                    acc + ("- Satélite " + (i + 1) + ". " + sat + "\n"),
+                  ""
+                )
+              : "Nenhum Satélite Cadastrado\n"
+          }\n`;
+        })
+      : (list += "Nenhum Planeta Cadastrado!");
 
     alert(list);
   }
