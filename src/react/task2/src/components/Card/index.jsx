974a57d9a696a9637fef4203b6ccb4b37d2978dd
Willian <williansilvap25@gmail.com>
2023-07-21 00:03:02 -0300

    add sass layout

--- src/react/task2/src/components/Card/index.jsx
+++ src/react/task2/src/components/Card/index.jsx
@@ -5,13 +5,13 @@ export const Card = () => {
   return (
     <>
       <div className={styles.card}>
-        <img src={cardImgStarWars} alt="Image Card" className='card-cardImg' />
-        <div className="card-details">
-          <h2 className="card-details-title">Pôster: Star Wars (1977)</h2>
-          <p className="card-details-description">
+        <img src={cardImgStarWars} alt="Image Card" className={`${styles.card} ${styles.img}`} />
+        <div className={`${styles.card} ${styles.details}`}>
+          <h2 className={`${styles.card} ${styles.details} ${styles.title}`}>Pôster: Star Wars (1977)</h2>
+          <p className={`${styles.card} ${styles.details} ${styles.description}`}>
             Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!
           </p>
-          <button className="card-details-button">Comprar agora</button>
+          <button className={`${styles.card} ${styles.details} ${styles.button}`}>Comprar agora</button>
         </div>
       </div>
     </>
