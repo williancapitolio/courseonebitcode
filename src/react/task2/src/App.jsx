1427c709345a0f1dfa4189e9d3cb06a52e6743aa
Willian <williansilvap25@gmail.com>
2023-07-21 22:28:15 -0300

    need some fix on css

--- src/react/task2/src/App.jsx
+++ src/react/task2/src/App.jsx
@@ -1,9 +1,17 @@
 import { Card } from "./components/Card";
 
+import styles from './styles/App.module.scss'
+
 export const App = () => {
   return (
     <>
-      <Card />
+      <div className={styles.app}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   )
 }
