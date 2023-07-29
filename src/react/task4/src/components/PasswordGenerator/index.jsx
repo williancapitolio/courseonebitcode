0e952f557ebff498c690c842a08861bb8fcc6902
Willian <williansilvap25@gmail.com>
2023-07-29 16:51:59 -0300

    missing send value to clipboard

--- src/react/task4/src/components/PasswordGenerator/index.jsx
+++ src/react/task4/src/components/PasswordGenerator/index.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react"
 import styles from "./styles.module.scss"
 
 export const PasswordGenerator = () => {
+  const [generateBtn, setGenerateBtn] = useState('Gerar')
+  const [password, setPassword] = useState('')
+  const [copyBtn, setCopyBtn] = useState('Copiar')
+
+  const handleGeneratePassword = () => {
+    setGenerateBtn('Gerado!')
+    setPassword('_YBw#RF&WHUM')
+  }
+
+  const handleCopyPassword = () => {
+    if (password.length > 0) {
+      setCopyBtn('Copiado!')
+
+    } else {
+      alert('Gere a senha primeiro!')
+    }
+  }
+
   return (
     <>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>Gerador de Senhas</h2>
         <div className={styles.sectionBtns}>
-          <button className={styles.sectionBtnsBtn}>Gerar!</button>
-          <button className={styles.sectionBtnsBtn}>Copiar</button>
+          <button onClick={handleGeneratePassword} className={styles.sectionBtnsBtn}>{generateBtn}</button>
+          <button onClick={handleCopyPassword} className={styles.sectionBtnsBtn}>{copyBtn}</button>
         </div>
-        <span className={styles.sectionResult}></span>
+        <span className={styles.sectionResult}>{password}</span>
       </section>
     </>
   )
