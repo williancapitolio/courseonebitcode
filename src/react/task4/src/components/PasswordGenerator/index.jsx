309d36da74df1735796e909a3b82e2796810e285
Willian <williansilvap25@gmail.com>
2023-07-31 22:59:08 -0300

    remove state for btn gerar

--- src/react/task4/src/components/PasswordGenerator/index.jsx
+++ src/react/task4/src/components/PasswordGenerator/index.jsx
@@ -2,14 +2,13 @@ import { useState } from "react"
 import styles from "./styles.module.scss"
 
 export const PasswordGenerator = () => {
-  const [generateBtn, setGenerateBtn] = useState('Gerar')
   const [password, setPassword] = useState('')
   const [copyBtn, setCopyBtn] = useState('Copiar')
 
   const handleGeneratePassword = () => {
     const random = Math.random().toString(36).slice(-10)
     setPassword(random)
-    setGenerateBtn('Gerado!')
+    setCopyBtn('Copiar')
   }
 
   const handleCopyPassword = () => {
@@ -29,7 +28,7 @@ export const PasswordGenerator = () => {
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>Gerador de Senhas</h2>
         <div className={styles.sectionBtns}>
-          <button onClick={handleGeneratePassword} className={styles.sectionBtnsBtn}>{generateBtn}</button>
+          <button onClick={handleGeneratePassword} className={styles.sectionBtnsBtn}>Gerar!</button>
           <button onClick={handleCopyPassword} className={styles.sectionBtnsBtn}>{copyBtn}</button>
         </div>
         <span className={styles.sectionResult}>{password}</span>
