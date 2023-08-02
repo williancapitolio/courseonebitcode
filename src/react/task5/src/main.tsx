ac123f6cf80380d9f10808af7fbdf33287b6125a
Willian <williansilvap25@gmail.com>
2023-08-01 23:49:37 -0300

    did some clear and add globals scss

--- src/react/task5/src/main.tsx
+++ src/react/task5/src/main.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import "./styles/globals.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
