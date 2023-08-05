0958e33653fc78cdd1fb2109d7123334a0a335f8
Willian <williansilvap25@gmail.com>
2023-08-05 17:36:19 -0300

    syntax correction

--- src/react/task5/src/hooks/useGetFullDate.ts
+++ src/react/task5/src/hooks/useGetFullDate.ts
@@ -2,9 +2,9 @@ export const useGetFullDate = () => {
   const newDate = new Date();
 
   const dateInTwoDigit = ("0" + newDate.getDate()).slice(-2);
-  const mounthInTwoDigit = ("0" + (newDate.getMonth() + 1)).slice(-2);
+  const monthInTwoDigit = ("0" + (newDate.getMonth() + 1)).slice(-2);
 
-  const date = `Em ${dateInTwoDigit}/${mounthInTwoDigit}/${newDate.getFullYear()}, ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
+  const date = `Em ${dateInTwoDigit}/${monthInTwoDigit}/${newDate.getFullYear()}, ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
 
   return { date };
 };
