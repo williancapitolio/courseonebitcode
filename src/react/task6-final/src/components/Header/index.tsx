9680630f624fb649d858f460fed39b546404c014
Willian <williansilvap25@gmail.com>
2023-08-09 22:26:23 -0300

    rename componant Navbar for Header

--- src/react/task6-final/src/components/Header/index.tsx
+++ src/react/task6-final/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
 
-export const Navbar = () => {
+export const Header = () => {
   return (
     <header className={styles.wrapper}>
       <Link className={styles.wrapperTitle} to="/">
--- src/react/task6-final/src/pages/Layout/index.tsx
+++ src/react/task6-final/src/pages/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import { Navbar } from "../../components/Navbar";
+import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 
 import styles from "./styles.module.scss";
@@ -8,7 +8,7 @@ import styles from "./styles.module.scss";
 export const Layout = () => {
   return (
     <>
-      <Navbar />
+      <Header />
       <main className={styles.wrapper}>
         <Outlet />
       </main>
