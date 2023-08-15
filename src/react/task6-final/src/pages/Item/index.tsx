63b481c30e9b137bdfd4f5c493294d8053d3b8aa
Willian <williansilvap25@gmail.com>
2023-08-14 22:33:15 -0300

    add html on single item page

--- src/react/task6-final/src/pages/Item/index.tsx
+++ src/react/task6-final/src/pages/Item/index.tsx
@@ -15,20 +15,56 @@ export const Item = () => {
   if (!item)
     return (
       <>
-        <Link to="/">Voltar</Link>
-        <h2>Produto não encontrado!</h2>
+        <section className={styles.wrapper}>
+          <Title title={"Stock Items"} />
+          <NavItems isAllActive={false} isNewActive={false} />
+          <h2>Produto não encontrado!</h2>
+          <Link to="/">Voltar</Link>
+        </section>
       </>
     );
 
   return (
     <>
       <section className={styles.wrapper}>
-      <Title title={"Stock Items"} />
-        <NavItems 
-          isAllActive={false}
-          isNewActive={false}
-        />
-        <p>{item.name}</p>
+        <Title title={"Stock Items"} />
+        <NavItems isAllActive={false} isNewActive={false} />
+        <div className={styles.wrapperHead}>
+          <h3 className={styles.wrapperHeadTitle}>{item.name}</h3>
+          <button className={styles.wrapperHeadUpdate}>Atualizar</button>
+          <button className={styles.wrapperHeadDelete}>Excluir</button>
+        </div>
+        <div className={styles.wrapperBody}>
+          <div className={styles.wrapperBodyCards}>
+            <div className={styles.wrapperBodyCardsCard}>
+              <span className={styles.wrapperBodyCardsCardItem}>
+                Categoria: {item.cat}
+              </span>
+            </div>
+            <div className={styles.wrapperBodyCardsCard}>
+              <span className={styles.wrapperBodyCardsCardItem}>
+                {" "}
+                Quantidade em estoque: {item.qtde}
+              </span>
+            </div>
+            <div className={styles.wrapperBodyCardsCard}>
+              <span className={styles.wrapperBodyCardsCardItem}>
+                Preço: R$ {item.price.toFixed(2)}
+              </span>
+            </div>
+          </div>
+        </div>
+        <p className={styles.wrapperDescription}>{item.desc}</p>
+        <div className={styles.wrapperFooter}>
+          <span className={styles.wrapperFooterDate}>
+            Cadastrado em: {item.createdAt}
+          </span>
+          {item.updatedAt && (
+            <span className={styles.wrapperFooterDate}>
+              Atualizado em: {item.updatedAt}
+            </span>
+          )}
+        </div>
       </section>
     </>
   );
